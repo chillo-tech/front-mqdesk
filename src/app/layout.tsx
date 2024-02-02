c180966ec34b4a25d8961e702aab35c44079c274
BrightkyEfoo <brightefoo@gmail.com>
2024-02-02 16:55:58 +0100

    ajout des analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Analytics } from "@/components/Analytics";
 import { ApplicationContext } from "./(user)/ApplicationContext";
 import useLayout from "./(user)/useLayout";
 import ApplicationProvider from "./ApplicationProvider";
@@ -10,8 +11,6 @@ const default_description =
   "Optimisez les performances de vos applications avec MQ Desk, la solution d'hébergement RabbitMQ de pointe. Notre service offre une plateforme robuste et fiable pour la gestion des messages, garantissant une communication efficace entre les composants de votre application. Avec une infrastructure hautement disponible, MQ Desk assure une intégration transparente de RabbitMQ, libérant ainsi votre équipe de développement des préoccupations liées à l'infrastructure";
 
 const default_title = "MQ Desk: rabbitmq hébergé pour vos applications";
-
-
 
 export default function RootLayout({
   children,
@@ -33,10 +32,13 @@ export default function RootLayout({
           </head>
           <body
             className={`${pjs.className} scroll-smooth  font-sans`}
-            onScroll={e => console.log('scroll')}
+            onScroll={(e) => console.log("scroll")}
           >
             {children}
           </body>
+          {process.env.NEXT_PUBLIC_ANALYTICS_KEY && (
+            <Analytics ga_id={process.env.NEXT_PUBLIC_ANALYTICS_KEY} />
+          )}
         </ApplicationContext.Provider>
       </ApplicationProvider>
     </html>
