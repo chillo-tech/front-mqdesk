804fdfec2f5908a013286a492292f025e77e1262
Achille <achille.mbougueng@chillo.tech>
2024-02-03 14:59:33 +0100

    Section espaces

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  output: 'standalone',
+  generateEtags: false,
+  env: {
+    NEXT_PUBLIC_BACKEND_API_KEY: process.env.NEXT_PUBLIC_BACKEND_API_KEY,
+    NEXT_PUBLIC_BACKEND_API_SECRET: process.env.NEXT_PUBLIC_BACKEND_API_SECRET,
+    NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI
+  },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_BACKEND_API_KEY: process.env.NEXT_PUBLIC_BACKEND_API_KEY,
+    NEXT_PUBLIC_BACKEND_API_SECRET: process.env.NEXT_PUBLIC_BACKEND_API_SECRET,
+    NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI
+  },
+}
 
 export default nextConfig;
