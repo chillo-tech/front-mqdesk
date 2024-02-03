8d44e9a89a3990050a92905938f1b2de47037ade
BrightkyEfoo <brightefoo@gmail.com>
2024-02-03 12:37:15 +0100

    correction des id

--- src/components/comment-ca-marche.tsx
+++ src/components/comment-ca-marche.tsx
@@ -10,7 +10,11 @@ type Props = {};
 function CommentCaMarche({}: Props) {
   const { containerRef } = useSetScrolltag();
   return (
-    <section id="how-it-works" className="bg-gray-200 py-10 md:py-20 text-lg text-light" ref={containerRef}>
+    <section
+      id="how-it-works"
+      className="bg-gray-200 py-10 md:py-20 text-lg text-light"
+      ref={containerRef}
+    >
       <div className="container text-center">
         <h2
           className={`${montserrat.className} text-3xl md:text-5xl font-bold text-center`}
