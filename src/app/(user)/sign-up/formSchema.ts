a3f94b33d90686fb6c2e9b6713b5c55d52928463
BrightkyEfoo <brightefoo@gmail.com>
2024-02-04 11:10:06 +0100

    gestion de l'interdependance entre phone et phoneIndex

--- src/app/(user)/sign-up/formSchema.ts
+++ src/app/(user)/sign-up/formSchema.ts
@@ -1,9 +1,24 @@
 import { transformToNull } from "@/utils";
 import * as yup from "yup";
 
-export const signUpSchema = yup.object().shape({
-  lastName: yup.string().required(),
-  email: yup.string().email().required(),
-  phoneIndex: yup.string().transform(transformToNull).nullable().optional(),
-  phone: yup.string().transform(transformToNull).nullable().optional(),
-});
+export const signUpSchema = yup.object().shape(
+  {
+    lastName: yup.string().required(),
+    email: yup.string().email().required(),
+    phoneIndex: yup
+      .string()
+      .transform(transformToNull)
+      .nullable()
+      .when(["phone"], ([phone], field, y) =>
+        phone ? field.required() : field
+      ),
+    phone: yup
+      .string()
+      .transform(transformToNull)
+      .nullable()
+      .when(["phoneIndex"], ([phoneIndex], field, y) =>
+        phoneIndex ? field.required() : field
+      ),
+  },
+  [["phone", "phoneIndex"]]
+);
